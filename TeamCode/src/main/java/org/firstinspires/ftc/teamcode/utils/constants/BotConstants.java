5a574102aec82ca3cbf7c61919df196f5fbf489a
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-12-27 17:12:37 -0500

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/BotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/BotConstants.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode.utils.constants;
 
+import static org.firstinspires.ftc.teamcode.utils.constants.AutoConstants.startX;
+import static org.firstinspires.ftc.teamcode.utils.constants.AutoConstants.startY;
+
 import com.acmerobotics.dashboard.config.Config;
 @Config
 public class BotConstants {
-    public static double goalX = 144;
-    public static double goalY = 144;
+    public static double goalX = 122.68;
+    public static double goalY = 123.84;
     public static double goalDY = 20; //correct later
     public static double CAMERA_YAW = 13;
     public enum BotState{
