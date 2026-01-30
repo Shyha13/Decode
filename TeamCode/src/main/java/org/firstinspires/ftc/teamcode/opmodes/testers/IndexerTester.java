09c35c02b331aebd7e3aab2e185f8f2e74037bc8
Aarav Dhawan <aaravdhawan25@gmail.com>
2026-01-29 20:31:19 -0500

    Indexer Tester and hood zeroing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testers/IndexerTester.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.opmodes.testers;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.robot.Robot;
+import org.firstinspires.ftc.teamcode.robot.commands.subsystemcommands.IndexerCommand;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Indexer;
+import org.firstinspires.ftc.teamcode.utils.MyTelem;
+
+@Config
+@TeleOp(name = "Indexer Tester", group = " ")
+public class IndexerTester extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MyTelem.init(telemetry);
+        Robot robot = new Robot(hardwareMap, false);
+        GamepadEx gp1 = new GamepadEx(gamepad1);
+        gp1.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenPressed(
+                new ParallelCommandGroup(
+                        new IndexerCommand(robot, Indexer.IndexState.OUT)
+                )
+        );
+        gp1.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenReleased(
+                new ParallelCommandGroup(
+                        new IndexerCommand(robot, Indexer.IndexState.IN)
+                )
+        );
+
+        waitForStart();
+        while (opModeIsActive()){
+            robot.update();
+        }
+        robot.stop();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/HoodConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/HoodConstants.java
@@ -5,7 +5,7 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class HoodConstants {
     public static double hoodServoPosition = 0;
-    public static double closeHoodAngle = 0;
+    public static double closeHoodAngle = 0.21;
     public static double openAngle = 0;
     public static double tuningTestingHoodPosition = 0;
 }
