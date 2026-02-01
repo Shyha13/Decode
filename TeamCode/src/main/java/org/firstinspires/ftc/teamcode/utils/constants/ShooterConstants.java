7f80b0b743f36686b062075b6aee23751e34a95e
Kennice Kwok <dharkarajveer@gmail.com>
2026-02-01 15:45:52 -0500

    Update ShooterConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/ShooterConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/ShooterConstants.java
@@ -8,11 +8,8 @@ public class ShooterConstants {
     public static double startingVelocity = 0.0;
     public static double speedingVelocity = 4000;
     public static double closeShootRPM = 4100;// 1800
-    public static double farShootRPM = 4100; //2.16k
     public static double tuningTestingRPM = 4100;
     public static double kf = 0.5, kp = 0.7, ki = 0, kd = 0.00000001;
-//    public static double CRkf = 0.3, CRkp = 0.004, CRki = 0, CRkd = 0.00001;
-//    public static int CR_TICKS_PER_REV = 28;
     public static int RPM_OFFSET = 50;
     public static int TICKS_PER_REV = 28;
     public static int MAX_RPM = 6000;
